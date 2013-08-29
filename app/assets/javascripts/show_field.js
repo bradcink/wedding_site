44249b5008fc24a7a65288692726c2b32375771b
Brad Cink <bradcink@me.com>
2013-08-28 22:06:46 -0700

    Modofied styling and added FAQ functionality.

--- app/assets/javascripts/show_field.js
+++ app/assets/javascripts/show_field.js
@@ -7,13 +7,20 @@ $(document).ready(function(){
                       $("#number_attending").slideUp("fast");
                         }
                       });
+                    $("input[name='user[rsvp_confirmation]']").on('click',function(){
+                    if($(this).val() == "1") {
+                      $("#number_attending-edit").slideDown("fast");
+                        }
+                    else{
+                      $("#number_attending-edit").slideUp("fast");
+                        }
+                      });
 
                     $(".btn-inverse").on('click',function(){
                         var divToToggle = $( $(this).find("a").attr('href') );
                         $(".simple-form:visible").not(divToToggle).hide();
                         divToToggle.slideToggle("fast");
                       });
-
                     $("#faq").on('click',function(){
                         $("#faq-field").show();
                         $("#edit-field").hide();
@@ -22,6 +29,10 @@ $(document).ready(function(){
                     $("#edit").on('click',function(){
                         $("#edit-field").show();
                         $("#faq-field").hide();
+                         return false;
+                      });
+                    $("#confirmation-reset").on('click',function(){
+                        $("#confirmation-reset-id").show();
                          return false;
                       });
                     });
