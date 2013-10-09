39288e3090b28cbab5ceb2b2e91de6ef27490cb0
Brad Cink <bradcink@me.com>
2013-10-08 23:55:19 -0700

    Production intent update 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ WeddingSite::Application.configure do
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
+  domain: "http://sooter-cink-wedding.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
