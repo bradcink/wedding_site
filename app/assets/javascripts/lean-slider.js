409a74c7b57a6bcf15b3606a1d410da307bcb53f
Brad Cink <bradcink@me.com>
2013-10-23 11:36:01 -0700

    Leanslider.js added

--- app/assets/javascripts/lean-slider.js
+++ app/assets/javascripts/lean-slider.js
@@ -48,3 +48,55 @@ function slideSwitch() {
 $(function() {
     setInterval( "slideSwitch()", 5000 );
 });
+
+function slideSwitch2() {
+    var $active = $('#slideshow2 IMG.active');
+    var $next = $active.next();    
+    
+    $next.addClass('active');
+    
+    $active.removeClass('active');
+}
+
+$(function() {
+    setInterval( "slideSwitch2()", 5000 );
+});
+
+function slideSwitch2() {
+    var $active = $('#slideshow2 IMG.active');
+    var $next = $active.next();
+
+    $active.addClass('last-active');
+        
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch2()", 5000 );
+});
+
+function slideSwitch2() {
+    var $active = $('#slideshow2 IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow2 IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow2 IMG:first');
+
+    $active.addClass('last-active');
+        
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch2()", 5000 );
+});
+
